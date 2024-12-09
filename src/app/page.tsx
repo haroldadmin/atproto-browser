138e5200f101bab7795a53a35f4055970b8d75fb
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2024-12-09 12:51:49 +0530

    Better utilise wide displays on home page (#13)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,13 @@ import LiveFeed from "@/components/live-feed/feed";
 
 export default function Home() {
   return (
-    <section>
-      <DIDForm />
-      <LiveFeed />
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+      <div className="col-span-1 lg:col-span-1">
+        <DIDForm />
+      </div>
+      <div className="col-span-1 lg:col-span-1">
+        <LiveFeed />
+      </div>
     </section>
   );
 }
