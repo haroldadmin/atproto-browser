f1860d1843a7e6260e0158d75c4420047b83ef15
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2025-05-25 09:44:12 +0100

    Fix URL scheme in sitemap urls (#25)

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,7 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   const publicRoutes: string[] = ["/", "/about"];
 
   return publicRoutes.map((route) => ({
-    url: `${SITE_URL}${route}`,
+    url: `https://${SITE_URL}${route}`,
     lastModified: new Date(),
     changeFrequency: "monthly",
     priority: 0.5,
