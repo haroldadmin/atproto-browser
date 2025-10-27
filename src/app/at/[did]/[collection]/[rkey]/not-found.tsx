8f66cdccffa4870b8fd91d94405722dee24586f6
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2025-10-27 21:35:55 +0000

    Add record not found page

---
+++ src/app/at/[did]/[collection]/[rkey]/not-found.tsx
@@ -0,0 +1,9 @@
+export default async function RecordNotFound() {
+  return (
+    <div className="my-8">
+      <p className="italic text-muted-foreground">
+        This record could not be found
+      </p>
+    </div>
+  );
+}
