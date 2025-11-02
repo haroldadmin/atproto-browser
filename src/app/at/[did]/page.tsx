ef9149c1c3c872b9804eadbb54cd4aa33c909581
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2025-11-02 15:10:25 +0000

    Add the ability to export a repository (#30)
    * Add the ability to export a repository
    
    * Run prettier

--- src/app/at/[did]/page.tsx
+++ src/app/at/[did]/page.tsx
@@ -1,10 +1,13 @@
 import CollectionsList from "@/components/collections-list";
+import { Download } from "lucide-react";
 import DIDDocument from "@/components/did-document";
 import RawRecord from "@/components/raw-record";
 import { Separator } from "@/components/ui/separator";
 import { cachedResolveDidDoc } from "@/lib/did";
 import { extractPDSUrl } from "@/lib/records";
 import { notFound } from "next/navigation";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default async function CollectionsPage({
   params,
@@ -24,6 +27,15 @@ export default async function CollectionsPage({
 
   return (
     <div>
+      <div className="flex flex-row justify-between items-center gap-4">
+        <h1 className="text-4xl font-bold">Repository</h1>
+        <Link href={`/at/${did}/export`} target="_blank">
+          <Button variant="ghost">
+            <Download />
+            Export
+          </Button>
+        </Link>
+      </div>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 my-4">
         <CollectionsList did={doc.id} pds={pdsUrl} />
         <div className="col-span-1 lg:col-span-2">
