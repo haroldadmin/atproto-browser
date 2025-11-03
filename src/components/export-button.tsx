4a1f5df08906908e5041dbcf6295fb1646f5f234
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2025-11-03 23:41:03 +0000

    Replace export API route with a PDS redirect (#32)
    * Replace /export route with a PDS redirect
    
    * Fix quotes

---
+++ src/components/export-button.tsx
@@ -0,0 +1,22 @@
+import { Download } from "lucide-react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+export type ExportButtonProps = {
+  did: string;
+  pds: string;
+};
+
+export default function ExportButton({ did, pds }: ExportButtonProps) {
+  const exportUrl = new URL("/xrpc/com.atproto.sync.getRepo", pds);
+  exportUrl.searchParams.append("did", did);
+
+  return (
+    <Link href={exportUrl.toString()} target="_blank">
+      <Button variant="ghost">
+        <Download />
+        Export
+      </Button>
+    </Link>
+  );
+}
