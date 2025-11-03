7efb689a37dac6225f7f19e33c4e092b6dcd8d58
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2025-11-03 23:32:02 +0000

    Replace /export route with a PDS redirect

--- src/app/at/[did]/page.tsx
+++ src/app/at/[did]/page.tsx
@@ -8,6 +8,7 @@ import { extractPDSUrl } from "@/lib/records";
 import { notFound } from "next/navigation";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import ExportButton from "@/components/export-button";
 
 export default async function CollectionsPage({
   params,
@@ -29,12 +30,7 @@ export default async function CollectionsPage({
     <div>
       <div className="flex flex-row justify-between items-center gap-4">
         <h1 className="text-4xl font-bold">Repository</h1>
-        <Link href={`/at/${did}/export`} target="_blank">
-          <Button variant="ghost">
-            <Download />
-            Export
-          </Button>
-        </Link>
+        <ExportButton did={doc.id} pds={pdsUrl} />
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 my-4">
         <CollectionsList did={doc.id} pds={pdsUrl} />
