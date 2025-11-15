33003c649c561ca4a53b42a7465223a20dc0f394
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2025-11-15 12:25:25 +0000

    Make AT URIs clickable in raw records (#40)

--- src/lib/uris.ts
+++ src/lib/uris.ts
@@ -1,8 +1,54 @@
 import { AtUri } from "@atproto/api";
 import { join } from "lodash";
+import type { ShikiTransformer } from "shiki";
+import { visit } from "unist-util-visit";
 
 export function atUriToBrowserUri(uri: AtUri): string {
   const { host, collection, rkey } = uri;
   const segments = join([host, collection, rkey], "/");
   return `/at/${segments}`;
+}
+
+/**
+ * A Shiki transform to make AT URIs clickable in syntax
+ * highlighted code.
+ */
+export const AtUriTransformer: ShikiTransformer = {
+  name: "at-uri-linker",
+  line: (node) => {
+    visit(node, ["text"], (element, _, parent) => {
+      if (element.type !== "text") {
+        return;
+      }
+
+      if (!element.value.startsWith(`"at://`)) {
+        return;
+      }
+
+      const uri = parseATURI(element.value.replaceAll(`"`, ""));
+      if (!uri || !parent) {
+        return;
+      }
+
+      parent.children = [
+        {
+          type: "element",
+          tagName: "a",
+          children: [element],
+          properties: {
+            class: "underline-offset-4 text-accent dark:text-foreground",
+            href: atUriToBrowserUri(uri),
+          },
+        },
+      ];
+    });
+  },
+};
+
+function parseATURI(value: string): AtUri | undefined {
+  try {
+    return new AtUri(value);
+  } catch {
+    return undefined;
+  }
 }
