5c07fa9468e94cbb2dc107dd782791884eca1151
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2025-11-25 15:49:06 +0530

    Add ability to click on DIDs

--- src/components/raw-record.tsx
+++ src/components/raw-record.tsx
@@ -1,12 +1,12 @@
 import { codeToHtml } from "shiki";
-import { AtUriTransformer } from "@/lib/uris";
+import { AtUriTransformer, DidTransformer } from "@/lib/uris";
 
 export default async function RawRecord({ record }: { record: object }) {
   const code = JSON.stringify(record, null, 2);
   const highlightedHtml = await codeToHtml(code, {
     lang: "json",
     theme: "github-dark-default",
-    transformers: [AtUriTransformer],
+    transformers: [AtUriTransformer, DidTransformer],
   });
 
   return (
--- src/lib/uris.ts
+++ src/lib/uris.ts
@@ -25,7 +25,7 @@ export const AtUriTransformer: ShikiTransformer = {
         return;
       }
 
-      const uri = parseATURI(element.value.replaceAll(`"`, ""));
+      const uri = parseATURI(unquote(element.value));
       if (!uri || !parent) {
         return;
       }
@@ -45,10 +45,80 @@ export const AtUriTransformer: ShikiTransformer = {
   },
 };
 
+/**
+ * DID rules:
+ * - Must start with "did:"
+ * - Must have a method name
+ * - Must have an identifier
+ * - Must not end with ":"
+ * @see https://www.w3.org/TR/did-core/#did-syntax
+ */
+const DID_REGEX = /^did:[a-z]+:[a-zA-Z0-9._:%-]*[a-zA-Z0-9._%-]$/;
+
+/**
+ * A Shiki transformer to make DIDs clickable in syntax
+ * highlighted code.
+ */
+export const DidTransformer: ShikiTransformer = {
+  name: "did-linker",
+  line: (node) => {
+    visit(node, ["text"], (element, _, parent) => {
+      if (element.type !== "text") {
+        return;
+      }
+
+      if (!parent) {
+        return;
+      }
+
+      const isDid = DID_REGEX.test(unquote(element.value));
+      if (!isDid) {
+        return;
+      }
+
+      const didUri = `/at/${unquote(element.value)}`;
+
+      parent.children = [
+        {
+          type: "element",
+          tagName: "a",
+          children: [element],
+          properties: {
+            class: "underline-offset-4 text-accent dark:text-foreground",
+            href: didUri,
+          },
+        },
+      ];
+    });
+  },
+};
+
 function parseATURI(value: string): AtUri | undefined {
   try {
     return new AtUri(value);
   } catch {
     return undefined;
   }
+}
+
+function unquote(value: string): string {
+  if (!value.length) {
+    return value;
+  }
+
+  if (value === `"`) {
+    return "";
+  }
+
+  let start = 0;
+  let end = value.length;
+  if (value.startsWith(`"`)) {
+    start = 1;
+  }
+
+  if (value.endsWith(`"`)) {
+    end = value.length - 1;
+  }
+
+  return value.slice(start, end);
 }
